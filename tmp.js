b70ab8f4fc332432aebc9ed1cc91d1d785fd3573
Liu Yufu <liuyufu192837465@gmail.com>
2018-08-19 11:42:31 +0800

    Add OOI for kancolle html5, replace mht homepage with online webpage due to unknown bugs

---
+++ tmp.js
@@ -0,0 +1,4 @@
+window.onload=function(){
+    this.document.getElementById('login_id').value='your_username';
+    this.document.getElementById('password').value='your_password';
+}
---
+++ 试制聚合H5网页〇一型改伍/init.lua
@@ -0,0 +1,13 @@
+user_permission={
+  [1]	= "ACCESS_COARSE_LOCATION" ;
+  [2]	= "ACCESS_FINE_LOCATION" ;
+  [3]	= "ACCESS_NETWORK_STATE" ;
+  [4]	= "ACCESS_WIFI_STATE" ;
+  [5]	= "INTERNET" ;
+  [6]	= "WRITE_EXTERNAL_STORAGE" ;
+  } ;
+template="tool"
+appcode="01"
+appname="试制聚合H5网页〇一型改五"
+appver="0.5"
+packagename="com.MyFusApp.Theropod.FusionH5"
